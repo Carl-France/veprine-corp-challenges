8547a266fd79d7008251efd7d9a1b8787180d28a
CarltonFrance <7145715+CarltonFrance@users.noreply.github.com>
2020-01-13 21:12:35 -0500

    small changes

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,29 +1,24 @@
-#include <iostream>
-#include <vector>
-
-int main (){
-  
-  //get a string to test from the user
   std::string word;
   std::cout << "\n"
-  			<< "**************************" << "\n"
+  	    << "**************************" << "\n"
             << "**    Palindrome Time   **" << "\n"
             << "**************************" << "\n\n"
             << "Type a word to test it: ";
   std::cin >> word;
 
-  //create the reversed string
+  //create the reversed word
   std::string reversed_word;
 
   for (int i = word.length() - 1; i >= 0; i--){
       reversed_word.push_back(word[i]);
   }
 
+  //test if word matches reversed word
   if (word == reversed_word){
   	std::cout << word << " IS a palindrome." << "\n";
   }
 
-  else {std::cout << word << " IS NOT a palindrome." << "\n";}
+  else {std::cout << word << " IS NOT a palindrome." << "\n\n";}
 
 	return 0;
 }
